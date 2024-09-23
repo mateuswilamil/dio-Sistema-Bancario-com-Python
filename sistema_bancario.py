b10c3a2814976f35dea84ca9b2c4166c18bb2f97
Mateus Wilamil <mateuswilamil@hotmail.com>
2024-09-22 21:32:40 -0300

    Adicionadas as funcionalidades e testes lógicos para depositos e saques.

--- sistema_bancario.py
+++ sistema_bancario.py
@@ -17,13 +17,45 @@ while True:
     opcao = input(menu)
 
     if opcao == 'd':
-        print('depositar')
+        valor_deposito = float(input('Digite o valor a ser depositado: '))
+        if valor_deposito > 0:
+            extrato += f'R$ +{valor_deposito:,.2f}\n'
+            saldo += valor_deposito
+        else: print('Digite um valor valido para deposito')
 
     elif opcao == 's':
-        print('sacar')
+        valor_sacar = float(input('Digite o valor a ser sacado: '))
+
+        limite_saque_atingido = LIMITE_SAQUES < 1
+        saldo_insuficiente = valor_sacar > saldo
+        valor_saque_superior_limite_diario = valor_sacar > LIMITE_POR_SAQUE
+
+        if limite_saque_atingido:
+            print('Número de saques já realizados atingiu o limite. Tente novamente amanhã.')
+            
+        elif saldo_insuficiente:
+            print(f'Valor desejado do saque maior que o saldo em conta. Saldo atual: R$ {saldo:,.2f}')
+
+        elif valor_saque_superior_limite_diario:
+            print('Valor desejado superior ao limite por saque')
+
+        elif valor_sacar > 0:
+            extrato += f'R$ -{valor_sacar:,.2f}\n'
+            saldo -= valor_sacar
+            LIMITE_SAQUES = LIMITE_SAQUES - 1
+        
+        else:
+            print('Valor informado para saque é invalido.')
+                
 
     elif opcao == 'e':
-        print('extrato')
+        print(f"""
+\n===== Extrato =====
+{'Não foram realizadas movimentações' if not extrato else extrato}  
+\n===== Saldo =====
+{saldo:,.2f}
+\n=================
+""")
 
     elif opcao == 'q':
         print('Obrigado!')
